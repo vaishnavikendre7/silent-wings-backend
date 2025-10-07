4bed3877bf975678a705e028dd8552410df3874c
Vaishnavi Kendre <kendrevs2005@gmail.com>
2025-10-07 19:51:50 +0530

    Initial backend setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Silent Wings Backend is running...");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
